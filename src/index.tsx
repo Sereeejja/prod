d31d7457fb3bd4f11b9ef19b50d8128b276932dc
Sereeejja <xrasst00@stud.fit.vutbr.cz>
2024-08-14 01:01:57 +0200

    main: refactor code to Feature-Sliced Design

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,8 @@
 import { createRoot } from 'react-dom/client';
 import React from "react";
-import App from "./App";
+import App from "./app/App";
 import {BrowserRouter} from "react-router-dom";
-import ThemeProvider from "./theme/ThemeProvider";
+import {ThemeProvider} from "app/providers/ThemeProvider";
 
 
 const container = document.getElementById('root');
