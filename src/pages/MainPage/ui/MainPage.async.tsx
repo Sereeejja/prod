659f39e960b12da459fb45865b16472a10cb77d9
Sereeejja <sergey54324@gmail.com>
2024-08-22 01:28:40 +0200

    main fix verified commits

--- src/pages/MainPage/ui/MainPage.async.tsx
+++ src/pages/MainPage/ui/MainPage.async.tsx
@@ -1,7 +1,6 @@
 import { lazy } from 'react';
 
 export const MainPageAsync = lazy(() => new Promise((res) => {
-    // @ts-ignore
     setTimeout(() => {
         // @ts-ignore
         res(import('./MainPage'));
