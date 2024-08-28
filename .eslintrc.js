ebbe4bbf4ecd62657791418100e0949d346d24cc
Sereeejja <sergey54324@gmail.com>
2024-08-28 18:09:46 +0200

    main fix eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -40,10 +40,21 @@ module.exports = {
         'no-underscore-dangle': 'off',
         '@typescript-eslint/ban-ts-comment': 'warn',
         '@typescript-eslint/no-unused-vars': 'off',
-        'i18next/no-literal-string': ['error', { markupOnly: true }],
+        'i18next/no-literal-string': [
+            'error',
+            { markupOnly: true, ignoreAttribute: ['data-testid'] },
+        ],
         'max-len': ['warn', { code: 100, tabWidth: 4, ignoreComments: true }],
     },
     globals: {
         __IS_DEV__: true,
     },
+    overrides: [
+        {
+            files: ['**/src/**/*.test.{ts,tsx}'],
+            rules: {
+                'i18next/no-literal-string': 'off',
+            },
+        },
+    ],
 };
