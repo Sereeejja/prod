32581b8ce08521fc2513e5049b4dee3ff45090b7
Sereeejja <sergey54324@gmail.com>
2024-09-06 20:47:36 +0200

    fix storybook tests

--- src/app/providers/StoreProvider/ui/StoreProvider.tsx
+++ src/app/providers/StoreProvider/ui/StoreProvider.tsx
@@ -2,19 +2,23 @@ import React, { ReactNode } from 'react';
 import { Provider } from 'react-redux';
 import { createReduxStore } from 'app/providers/StoreProvider/config/store';
 import { StateSchema } from 'app/providers/StoreProvider/config/StateSchema';
+import { ReducersList } from 'shared/lib/components/DynamicModuleLoader/DynamicModuleLoader';
+import { ReducersMapObject } from '@reduxjs/toolkit';
 
 interface StoreProviderProps {
     children?: ReactNode;
     initialState?: Partial<StateSchema>;
+    asyncReducers?: Partial<ReducersMapObject<StateSchema>>;
 }
 
 const StoreProvider:React.FC<StoreProviderProps> = (props: StoreProviderProps) => {
     const {
         children,
         initialState,
+        asyncReducers,
     } = props;
 
-    const store = createReduxStore(initialState as StateSchema);
+    const store = createReduxStore(initialState as StateSchema, asyncReducers as ReducersMapObject<StateSchema>);
 
     return (
         <Provider store={store}>
--- src/shared/config/storybook/StoreDecorator/StoreDecorator.tsx
+++ src/shared/config/storybook/StoreDecorator/StoreDecorator.tsx
@@ -1,9 +1,15 @@
 import React from 'react';
 import { StoryFn } from '@storybook/react';
 import { StateSchema, StoreProvider } from 'app/providers/StoreProvider';
+import { ReducersMapObject } from '@reduxjs/toolkit';
+import { loginReducer } from 'features/AuthByUsername/model/slice/loginSlice';
 
-export const StoreDecorator = (initialState: Partial<StateSchema>) => (Story: StoryFn) => (
-    <StoreProvider initialState={initialState}>
+const defaultAsyncReducers: Partial<ReducersMapObject<StateSchema>> = {
+    loginForm: loginReducer,
+};
+
+export const StoreDecorator = (initialState: Partial<StateSchema>, asyncReducers: Partial<ReducersMapObject<StateSchema>>) => (Story: StoryFn) => (
+    <StoreProvider initialState={initialState} asyncReducers={{ ...defaultAsyncReducers, ...asyncReducers }}>
         <Story />
     </StoreProvider>
 );
